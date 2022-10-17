64574baa10b7798d93069d22496ce1a69a964058
izzy klein <izzy.kle@gmail.com>
2022-10-17 20:23:02 +0200

    Initial commit

---
+++ app/src/main/java/id/izzy/validator/MainActivity.java
@@ -0,0 +1,14 @@
+package id.izzy.validator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
